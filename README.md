1ad1ab5a3b1f475911a213f209a357e547902590
Florian Tischler <florian.tischler@gmx.net>
2018-09-11 14:03:25 +0200

    untested script

---
+++ README.md
@@ -0,0 +1,5 @@
+#Script for NPM Projects to update and preserve a dependency with yarn
+
+Usage: 
+
+``yarn start [path-to-package.json] [dependency name]`` e.g.: ``yarn start package.json @angular/core``
--- index.ts
+++ index.ts
@@ -1,7 +1,54 @@
-import { resolve } from 'path';
+import { join, resolve } from 'path';
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { DependencyMap, PackageJSON } from './package-json';
+import { omit } from 'lodash';
+import { spawnSync, SpawnSyncReturns } from 'child_process';
 
 const fileArg: string = process.argv[2] || 'package.json';
 const depArg: string | null = process.argv[3] || null;
 const filePath: string = resolve(fileArg);
 
-console.log(filePath);
+if (!depArg) {
+  throw new Error('Please add a dependency');
+}
+
+const pathToDep: string = join('node_modules', filePath);
+const packageJSONPojo: PackageJSON  = JSON.parse(readFileSync(filePath).toString('utf-8'));
+
+let existsWhere: 'dependencies' | 'devDependencies' | null;
+
+if (packageJSONPojo.dependencies && Object.values(packageJSONPojo.dependencies).includes(depArg)) {
+  existsWhere = 'dependencies';
+} else if (packageJSONPojo.devDependencies && Object.values(packageJSONPojo.devDependencies).includes(depArg)) {
+  existsWhere = 'devDependencies';
+} else {
+  existsWhere = null;
+}
+
+if (!existsWhere) {
+  throw new Error('Dependency not found in package-json');
+}
+
+const dir: Buffer[] = readdirSync(pathToDep, {encoding: 'buffer'});
+const deps: DependencyMap | null = packageJSONPojo[existsWhere] || null;
+
+if (!deps) {
+  throw new Error('Dependency not found in package-json');
+}
+
+const tempPackageJSONPojo: PackageJSON = {
+  ...packageJSONPojo,
+  [existsWhere]: omit(deps, depArg)
+};
+
+writeFileSync(filePath, tempPackageJSONPojo);
+const yarnCP: SpawnSyncReturns<Buffer> = spawnSync(`cd ${filePath.replace('/package.json', '')} && yarn`);
+
+if (!yarnCP.error) {
+  writeFileSync(pathToDep, dir);
+  writeFileSync(filePath, packageJSONPojo);
+  const hasError: boolean = !!spawnSync('git checkout yarn.lock -f').error;
+  process.exit(hasError ? 1 : 0);
+} else {
+  process.exit(1);
+}
