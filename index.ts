c533d8fb67b6bb75a799a29b8ed67782823814ce
Florian Tischler <florian.tischler@gmx.net>
2018-09-11 15:48:02 +0200

    v1

--- index.ts
+++ index.ts
@@ -1,25 +1,35 @@
 import { join, resolve } from 'path';
-import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { DependencyMap, PackageJSON } from './package-json';
 import { omit } from 'lodash';
-import { spawnSync, SpawnSyncReturns } from 'child_process';
+import { promisify } from 'util';
+import { exec as _exec } from 'child_process';
+import { ncp } from 'ncp';
+import * as _rimraf from 'rimraf';
 
-const fileArg: string = process.argv[2] || 'package.json';
+const exec = promisify(_exec);
+const copy = promisify(ncp);
+const rimraf = promisify(_rimraf);
+
+const packageJSONArg: string = process.argv[2] || 'package.json';
 const depArg: string | null = process.argv[3] || null;
-const filePath: string = resolve(fileArg);
+const packageJSONPath: string = resolve(packageJSONArg);
+const rootPath: string = packageJSONPath.replace('/package.json', '');
 
 if (!depArg) {
   throw new Error('Please add a dependency');
 }
 
-const pathToDep: string = join('node_modules', filePath);
-const packageJSONPojo: PackageJSON  = JSON.parse(readFileSync(filePath).toString('utf-8'));
+const copyDestinationPath = join(rootPath, depArg.split('/')[0]);
+const depPath: string = join(rootPath, 'node_modules', ...depArg.split('/'));
+
+const packageJSONPojo: PackageJSON  = JSON.parse(readFileSync(packageJSONPath).toString('utf-8'));
 
 let existsWhere: 'dependencies' | 'devDependencies' | null;
 
-if (packageJSONPojo.dependencies && Object.values(packageJSONPojo.dependencies).includes(depArg)) {
+if (packageJSONPojo.dependencies && Object.keys(packageJSONPojo.dependencies).includes(depArg)) {
   existsWhere = 'dependencies';
-} else if (packageJSONPojo.devDependencies && Object.values(packageJSONPojo.devDependencies).includes(depArg)) {
+} else if (packageJSONPojo.devDependencies && Object.keys(packageJSONPojo.devDependencies).includes(depArg)) {
   existsWhere = 'devDependencies';
 } else {
   existsWhere = null;
@@ -29,7 +39,6 @@ if (!existsWhere) {
   throw new Error('Dependency not found in package-json');
 }
 
-const dir: Buffer[] = readdirSync(pathToDep, {encoding: 'buffer'});
 const deps: DependencyMap | null = packageJSONPojo[existsWhere] || null;
 
 if (!deps) {
@@ -41,14 +50,13 @@ const tempPackageJSONPojo: PackageJSON = {
   [existsWhere]: omit(deps, depArg)
 };
 
-writeFileSync(filePath, tempPackageJSONPojo);
-const yarnCP: SpawnSyncReturns<Buffer> = spawnSync(`cd ${filePath.replace('/package.json', '')} && yarn`);
-
-if (!yarnCP.error) {
-  writeFileSync(pathToDep, dir);
-  writeFileSync(filePath, packageJSONPojo);
-  const hasError: boolean = !!spawnSync('git checkout yarn.lock -f').error;
-  process.exit(hasError ? 1 : 0);
-} else {
-  process.exit(1);
-}
+(async () => {
+  mkdirSync(copyDestinationPath);
+  await copy(depPath, join(rootPath, ...depArg.split('/'))).catch(Promise.resolve);
+  writeFileSync(packageJSONPath, JSON.stringify(tempPackageJSONPojo, null, 2));
+  await exec(`cd ${rootPath} && yarn`);
+  writeFileSync(packageJSONPath, JSON.stringify(packageJSONPojo, null, 2));
+  await exec('git checkout yarn.lock -f');
+  await copy(copyDestinationPath, join(rootPath,'node_modules')).catch(Promise.resolve);
+  await rimraf(copyDestinationPath).catch(Promise.resolve);
+})();
